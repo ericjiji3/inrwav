0649ffb9be0e42323a5b088ae9e4976b43b6d1c8
ericjiji3 <69319444+ericjiji3@users.noreply.github.com>
2022-06-09 00:14:03 -0500

    figuree some stuff out

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from "./pics/inrwav-logo.png";
 import MainMenu from './pages/MainMenu.js';
 import Navbar from './components/Navbar.js';
 import './App.css';
@@ -7,17 +7,44 @@ import startingScreen from "./pics/startingScreen.mp4";
 import stars from "./pics/stars.gif";
 import clickToContinue from "./pics/clickToContinue.mp4";
 import hello from "./pics/hello.mp4";
+import React, {Component,useState, useEffect, useRef} from 'react';
 
-function App() {
+
+function App(){
+  const [reveal, setReveal] = useState(false);
+  const [shatter, setShatter] = useState(false);
+  const revealRef = useRef(reveal);
+  revealRef.current = reveal;
+
+  useEffect(() => {
+    function clickedFunction(){
+      setShatter(true);
+      setReveal(false);
+    }
+    setTimeout(() => {
+      setReveal(true); // count is 0 here
+    }, 3000);
+    setTimeout(() =>{
+      document.addEventListener('click', clickedFunction)
+  }, 4500)
+  }, []);
+
+    
   return (
     <div className="App">
       <div className="starting-screen ">
-        {/* <video autoPlay loop muted>
+        
+        <div className="video-container">
+        <video autoPlay loop muted>
           <source src={startingScreen} type="video/mp4"/>
-        </video> */}
-        <video autoPlay>
-          <source src={hello} type="video/mp4"/>
         </video>
+        </div>
+        <div className="logo-description">
+          <img className="logo" src={logo} alt="oops!"/>
+          <h2 className={reveal ? 'show':'hide'}>Press to Continue</h2>
+          
+        </div>
+        <div className="noise-filter"></div>
       </div>
       {/* <MainMenu/> */}
     </div>
